3cec1669bf93844c88a66e4f08c0a5b4d54f71b5
Musta Rohman <mustarohman@mustas-imac.home>
2019-02-21 10:49:35 +0000

    removed commented code

--- src/lib/ngx-tutorial-overlay.directive.ts
+++ src/lib/ngx-tutorial-overlay.directive.ts
@@ -12,22 +12,6 @@ export class NgxTutorialOverlayDirective {
         private viewContainerRef: ViewContainerRef,
         private componentFactoryResolver: ComponentFactoryResolver
     ) {
-        // this.wrapper = document.createElement('div');
-        // this.wrapper.setAttribute('style', `
-        // background-color: red; z-index: 15; position: absolute;
-        // position: fixed;
-        // top: 0;
-        // left: 0;
-        // right: 0;
-        // bottom: 0;
-        // width: 100%;
-        // height: 100%;
-        // display: block;
-        // background-color: rgba(0, 0, 0, 0.589);
-        // `);
-        // const parent = this.el.nativeElement.parentElement;
-        // parent.prepend(this.wrapper);
-        // console.log(parent);
         const compFactory = this.componentFactoryResolver.resolveComponentFactory(NgxTutorialOverlayComponent);
         const compRef = viewContainerRef.createComponent(compFactory);
         compRef.instance.list = [{test: 'test'}];
