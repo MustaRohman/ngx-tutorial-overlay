46282b8f9d91dd79495f40cc8e3df0f28350747b
Musta Rohman <mustarohman@gmail.com>
2019-03-16 13:00:24 +0000

    added positioning to overlay-label comp

--- projects/ngx-tutorial-overlay/src/lib/components/ngx-overlay-label/ngx-overlay-label.component.ts
+++ projects/ngx-tutorial-overlay/src/lib/components/ngx-overlay-label/ngx-overlay-label.component.ts
@@ -10,6 +10,8 @@ export class NgxOverlayLabelComponent implements OnInit {
   @Input() title: string;
   @Input() description: string;
   @Input() position: 'above' | 'below' | 'left' | 'right';
+  @Input() top: number;
+  @Input() left: number;
 
   constructor() { }
 
