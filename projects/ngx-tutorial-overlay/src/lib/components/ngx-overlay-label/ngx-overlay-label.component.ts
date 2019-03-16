ee64017eef3dc81f9ce59740cca9ae5a9d1bc719
Musta Rohman <mustarohman@gmail.com>
2019-03-16 14:33:04 +0000

    added change detection for top and left inputs

--- projects/ngx-tutorial-overlay/src/lib/components/ngx-overlay-label/ngx-overlay-label.component.ts
+++ projects/ngx-tutorial-overlay/src/lib/components/ngx-overlay-label/ngx-overlay-label.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'lib-ngx-overlay-label',
@@ -10,8 +10,20 @@ export class NgxOverlayLabelComponent implements OnInit {
   @Input() title: string;
   @Input() description: string;
   @Input() position: 'above' | 'below' | 'left' | 'right';
-  @Input() top: number;
-  @Input() left: number;
+  @Input() set top(value: number) {
+    console.log('OverlayLabel.top', value);
+    this._top = value;
+  }
+  @Input() set left(value: number) {
+    console.log('OverlayLabel.left', value);
+    this._left = value;
+  }
+
+  _title: string;
+  _description: string;
+  _position: 'above' | 'below' | 'left' | 'right';
+  _top: number;
+  _left: number;
 
   constructor() { }
 
