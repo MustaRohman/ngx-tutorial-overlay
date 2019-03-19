c7156433cc885b17ace0307ac9670f4e6f4a601d
Musta Rohman <mustarohman@gmail.com>
2019-03-19 09:54:26 +0000

    updated public_api.ts to reflect new components

--- projects/ngx-tutorial-overlay/src/public_api.ts
+++ projects/ngx-tutorial-overlay/src/public_api.ts
@@ -4,5 +4,8 @@
 
 export * from './lib/ngx-tutorial-overlay.service';
 export * from './lib/components/ngx-tutorial-overlay/ngx-tutorial-overlay.component';
+export * from './lib/components/ngx-overlay-label/ngx-overlay-label.component';
+export * from './lib/components/ngx-overlay-arrow/ngx-overlay-arrow.component';
 export * from './lib/ngx-tutorial-overlay.directive';
+export * from './lib/ngx-tutorial-item.directive';
 export * from './lib/ngx-tutorial-overlay.module';
